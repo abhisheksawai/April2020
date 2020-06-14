326f2220deabfddd6a51a3dda2d202aecffc9d47
Abhishek <sawai.abhishek@gmail.com>
2020-06-14 12:44:07 +0530

    deleted and added anoterh link to check

--- April2020/src/abstractdoubt/CheckGit.java
+++ April2020/src/abstractdoubt/CheckGit.java
@@ -5,7 +5,7 @@ public class CheckGit {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println("abhishek added this code");
+		System.out.println("deleted and added anoterh link");
 
 	}
 
