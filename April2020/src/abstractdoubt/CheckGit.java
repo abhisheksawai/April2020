84b862985742b8427690df749ae28ce7fc012ac1
Abhishek <sawai.abhishek@gmail.com>
2020-06-14 12:42:54 +0530

    added new func of login check git

---
+++ April2020/src/abstractdoubt/CheckGit.java
@@ -0,0 +1,12 @@
+package abstractdoubt;
+
+public class CheckGit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("abhishek added this code");
+
+	}
+
+}
